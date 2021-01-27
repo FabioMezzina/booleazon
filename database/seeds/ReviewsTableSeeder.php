abd7701545883b5aa88137f64419658f73881952
Marco Giulietti <ga6t00n@gmail.com>
2021-01-27 18:00:50 +0100

    Seeder di Specs e Review

--- database/seeds/ReviewsTableSeeder.php
+++ database/seeds/ReviewsTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Product;
+use App\Review;
 
 class ReviewsTableSeeder extends Seeder
 {
@@ -9,8 +12,23 @@ class ReviewsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $products = Product::all();
+
+        foreach ($products as $product) {
+            
+            for ($i=0; $i < 4; $i++) { 
+                
+                $newReview = new Review();
+
+                $newReview->product_id = $product->id;
+                $newReview->rating = $faker->randomFloat(0, 1, 5);
+                $newReview->author = $faker->text(20);
+                $newReview->body = $faker->paragraphs(1, true);
+
+                $newReview->save();
+            }
+        }
     }
 }
--- database/seeds/SpecsTableSeeder.php
+++ database/seeds/SpecsTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Product;
+use App\Spec;
 
 class SpecsTableSeeder extends Seeder
 {
@@ -9,8 +12,29 @@ class SpecsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $products = Product::all();
+
+        foreach ($products as $product) {
+            
+            $newSpec = new Spec();
+
+            $newSpec->product_id = $product->id;
+            $newSpec->category = $faker->text(50);
+            $newSpec->genre = $faker->text(10);
+            $newSpec->handlebar = $faker->text(15);
+            $newSpec->saddle = $faker->text(15);
+            $newSpec->wheels = $faker->text(15);
+            $newSpec->tires = $faker->text(15);
+            $newSpec->fenders = $faker->text(15);
+            $newSpec->light = $faker->text(15);
+            $newSpec->electrical = $faker->boolean();
+            $newSpec->brakes = $faker->text(15);
+            $newSpec->gear = $faker->text(15);
+
+            $newSpec->save();
+
+        }
     }
 }
