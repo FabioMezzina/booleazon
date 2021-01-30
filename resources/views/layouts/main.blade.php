2dfd20a43852e48988554d6c0c95c1962389b6a9
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2021-01-30 13:22:58 +0100

    fix main title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,11 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
+        <title>Bikeazon</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
     </head>
     <body>
 
@@ -15,7 +12,6 @@
 
         <div class="container">
             @yield('content')
-
         </div>
 
         @include ('partials/footer')
